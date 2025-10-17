05cad7ce0abcfa21f0f1dad1b7fde07b1bd212d4
tritle129-lgtm <tritle129@gmail.com>
2025-10-17 23:38:06 +0700

    Update vite.config.ts add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/tritle129-lgtm.github.io/', // 👈 เปลี่ยนให้ตรงกับชื่อ repo
+})
+
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
